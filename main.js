b509a9a363f46b041567aec4e2dcf2459a2c9a95
MakerSquare Student <students@themakersquare.com>
2013-11-24 20:35:51 -0600

    project complete

--- main.js
+++ main.js
@@ -6,73 +6,83 @@ var spaces = [
   NaN, NaN, NaN
 ];
 
-var player1 = 'veggies';
-var player2 = 'junkfood';
-var currentPlayer = null;
-var gameWon = false
 
-var setNextTurn = function () {
-  if (currentPlayer === player1) {
-    currentPlayer = player2;
-  }
-  else {
-    currentPlayer = player1;
-  }
-  $('#turn-label').text(currentPlayer);
-};
+$("#playerOneButton").click(function () {
+  $( ".line" ).hide();
+  $('#turn-label').html($('#playerOneName').val());
+  var player1 = $('#playerOneName').val();
+  var player2 = $('#playerTwoName').val();
+  var currentPlayer = null;
+  var gameWon = false;
+  var veggie_wins = 0
+  var junkfood_wins = 0
 
-var checkForWinner = function () {
-  // Because (NaN === NaN) is always false, we can safely assume
-  // that if three spaces in a row are the same, all three spaces are
-  // marked by a player, and not all empty.
-
-  if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
-    || spaces[3] === spaces[4] && spaces[4] === spaces[5]
-    || spaces[6] === spaces[7] && spaces[7] === spaces[8]
-    // TODO: Check for rest of game winning cases
-    // TODO: vertical wins
-    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
-    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
-    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
-    // TODO: diagonal wins
-    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
-    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
-  )
-  {
-    console.log(currentPlayer+' won');
-    // TODO: Trigger 'game-win' event with the winning player as the event data
-    $(document).trigger('game-win', currentPlayer);
-  }
-};
-
-$(document).on('click', '#board .space', function (e) {
- if (!gameWon) {
-    var spaceNum = $(e.currentTarget).index();
-    console.log('You clicked on space #' + spaceNum);
-      // Marks the space with the current player's name
-      // TODO: Don't mark it unless the space is blank
-    if (spaces[spaceNum]) {
-      alert("this place is full");
-      setNextTurn();
-    } else{
-      spaces[spaceNum] = currentPlayer;
-    $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
-    }; 
-      // Adds a class to elem so css can take care of the visuals
-    checkForWinner();
-    setNextTurn();
+  var setNextTurn = function () {
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    }else {
+      currentPlayer = player1;
+    }
+    $('#turn-label').text('It\'s ' +currentPlayer+'\'s turn');
   };
-});
 
+  var checkForWinner = function () {
+    if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
+      || spaces[3] === spaces[4] && spaces[4] === spaces[5]
+      || spaces[6] === spaces[7] && spaces[7] === spaces[8]
+      || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+      || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+      || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+      || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+      || spaces[2] === spaces[4] && spaces[4] === spaces[6]
+    ){
+      if (currentPlayer === player1) {
+        veggie_wins+=1;
+        $('#veggie_wins').text(player1+ ' wins: '+ veggie_wins);
+      } else{
+        junkfood_wins+=1;
+        $('#junkfood_wins').text(player2+ ' wins: '+ junkfood_wins);
+      };
+      console.log(currentPlayer+' won');
+      $(document).trigger('game-win', currentPlayer);
+    };
+  };
 
-$(document).on('game-win', function (e, winner) {
-  // TODO: Alert who won the game
-  alert(winner+" won!");
-  gameWon = true
-});
+  $(document).on('click', '#board .space', function (e) {
+   if (!gameWon) {
+      var spaceNum = $(e.currentTarget).index();
+      console.log('You clicked on space #' + spaceNum);
+      if (spaces[spaceNum]) {
+        alert("this place is full");      
+      } else {
+        spaces[spaceNum] = currentPlayer;
+        if (currentPlayer===player1) {
+          $('#board .space:eq(' + spaceNum + ')').addClass('veggies');
+        } else{
+          $('#board .space:eq(' + spaceNum + ')').addClass('junkfood');
+        };
+        checkForWinner();
+        setNextTurn();
+      };  
+    };
+  });
 
-// Start the game
+  $(document).on('game-win', function (e, winner) {
+    // TODO: Alert who won the game
+    alert(winner+" won!");
+    gameWon = true;
+  });
+
+  // Start the game
+  $('.new_game').click(function (e) {
+    gameWon = false;
+    $('#board .space').removeClass('veggies');
+    $('#board .space').removeClass('junkfood');
+    for (var i = 8; i >= 0; i--) {  
+      spaces[i]= NaN   
+    }; 
+  });
 
   setNextTurn();
-
+});
 
